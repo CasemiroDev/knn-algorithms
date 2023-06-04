5a77bb08e8ac4674ec8a3ffd496d5795f650cf7a
Gustavo Casemiro <gustavo.casemiro1@gmail.com>
2023-06-04 19:32:45 -0300

    end commit

--- knn - classifier/main.py
+++ knn - classifier/main.py
@@ -7,12 +7,22 @@ df = pd.read_csv("knn - classifier/database.csv")
 X = df.drop('risco', axis=1)
 y = df.risco
 
-# Avaliando o Classificador e aplicando a predição
+# Importando as principais bibliotecas
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=2/3)
+# Pré-processamento dos Inputs
+from sklearn.preprocessing import MinMaxScaler
+normalizador = MinMaxScaler()
+X_norm = normalizador.fit_transform(X)
+
+# Avaliando o Classificador e aplicando a predição
+X_train, X_test, y_train, y_test = train_test_split(X_norm, y, train_size=2/3)
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(X_train, y_train)
 print(accuracy_score(y_test,knn.predict(X_test)))
+
+novo_cliente = [[18,800]]
+X_new = normalizador.transform(novo_cliente)
+print(knn.predict(X_new))
